3843f2201a96b72f45c956db40d032f2d7101515
Yantrue <riyansanjaya703@gmail.com>
2026-04-27 10:06:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Kalkulator PHP</title>
+    <title>Kalkulator</title>
 </head>
 <body>
 
 <h2>Kalkulator</h2>
 
-<form method="post">
-    <input type="text" name="angka1" placeholder="Masukkan angka 1"><br><br>
-    <input type="text" name="angka2" placeholder="Masukkan angka 2"><br><br>
+<form method="POST">
+    <input type="number" name="a" placeholder="Angka 1" required><br><br>
+    <input type="number" name="b" placeholder="Angka 2" required><br><br>
 
-    <select name="operasi">
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-    </select><br><br>
-
-    <button type="submit">Hitung</button>
+    <button name="tambah">+</button>
+    <button name="kurang">-</button>
+    <button name="kali">*</button>
+    <button name="bagi">/</button>
 </form>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $a = (float) $_POST["angka1"];
-    $b = (float) $_POST["angka2"];
-    $op = $_POST["operasi"];
-    $hasil = 0;
-
-    if ($op == "+") {
-        $hasil = $a + $b;
-    } elseif ($op == "-") {
-        $hasil = $a - $b;
-    } elseif ($op == "*") {
-        $hasil = $a * $b;
-    } elseif ($op == "/") {
-        if ($b == 0) {
-            echo "Tidak bisa dibagi 0";
-            exit;
-        }
-        $hasil = $a / $b;
+if(isset($_POST['tambah'])){
+    echo "Hasil: " . ($_POST['a'] + $_POST['b']);
+}
+if(isset($_POST['kurang'])){
+    echo "Hasil: " . ($_POST['a'] - $_POST['b']);
+}
+if(isset($_POST['kali'])){
+    echo "Hasil: " . ($_POST['a'] * $_POST['b']);
+}
+if(isset($_POST['bagi'])){
+    if($_POST['b'] == 0){
+        echo "Tidak bisa bagi 0";
+    } else {
+        echo "Hasil: " . ($_POST['a'] / $_POST['b']);
     }
-
-    echo "<h3>Hasil: $hasil</h3>";
 }
 ?>
 
